a89bab951e4143a93db0d502df4827255be41ac7
Chase <zora1230@gmail.com>
2022-03-17 00:07:42 -0700

    comments.
    -Chase

--- app.js
+++ app.js
@@ -3,19 +3,19 @@ const mysql = require('mysql');
 const fs = require('fs');
 
 
-
+//Database file names
 const con = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'Maggie' 
 });
-
+//database connetion 
 con.connect(function(err){
   if (err) throw err;
   console.log("Connected to Database")
 })
 
-
+//starting the server and will let us know in terminal if the server connects successfully
 http.createServer(function (req, res) {
   
 }).listen(8080, function(error){
