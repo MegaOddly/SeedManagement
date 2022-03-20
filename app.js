cc407edb35440db1a05c97027f875245e0b295ae
gavj12 <100880311+gavj12@users.noreply.github.com>
2022-03-20 12:35:07 -0700

    app.js update

--- app.js
+++ app.js
@@ -1,28 +1,53 @@
-const http = require('http');
-var mysql = require('mysql');
-const fs = require('fs');
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
+const mysql = require('mysql');
+// const { Http2ServerRequest } = require('http2');
 
+dotenv.config({ path: './.env'})
 
-//Database file names
-var con = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Maggie' 
-});
-//database connetion 
-con.connect(function(err){
-  if (err) throw err;
-  console.log("Connected to Database")
-});
+const app = express();
 
-//starting the server and will let us know in terminal if the server connects successfully
-http.createServer(function (req, res) {
-  
-}).listen(8080, function(error){
-  if (error) {
-    console.log('Error connecting to server', error)
-  }else {
-    console.log('server is listening on port 8080')
-  }
+const db = mysql.createConnection({
+  host : process.env.DATABASE_HOST,
+  user : process.env.DATABASE_USER,
+  password :  process.env.DATABASE_PASSWORD, 
+  database :  process.env.DATABASE
 });
 
+const publicDirectory = path.join(__dirname, './public');
+app.use(express.static(publicDirectory));
+
+app.set('view engine', 'hbs');
+
+db.connect(function(error){
+  if (error) throw error
+  else console.log('MYSQL is connected...')
+});
+
+// db.connect (function(err){
+//   if (error) {
+//     console.log(err)
+//   } else {
+//     console.log("MYSQL Connected...")
+//   }
+// })
+
+// app.get('/createdb', (req, res) => {
+//   let sql ='CREATE DATABASE projectsql';
+//   db.query(sql, () => {
+//     if(error) throw err;
+//     console.log(result);
+//     res.send('database created...');
+//   })
+// })
+
+//open login page...this will be home page.
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, '/HTML/login.html'))
+});
+
+//host
+app.listen(8080, () => {
+  console.log("Server started on port 8080");
+});
